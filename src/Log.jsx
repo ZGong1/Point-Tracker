4895440e0c8074916d94a89ae8aa539d2a9cc788
Eric <ericpeterson@tamu.edu>
2024-07-19 22:11:21 -0500

    log page looks a (little) better

--- src/Log.jsx
+++ src/Log.jsx
@@ -1,3 +1,5 @@
+import "./log.css"
+
 const Log = ( {} ) => {
     var logInfo = localStorage.getItem("log")
     logInfo = JSON.parse(logInfo)
@@ -6,7 +8,7 @@ const Log = ( {} ) => {
         <div>
             {/* gross log info reversal */}
             {(logInfo.map( (item, idx) => logInfo[logInfo.length - 1 - idx]))
-                     .map( (item, idx) => <p key={idx}>{item.date}<br/>{item.info}</p>)}
+                     .map( (item, idx) => <p className="logItem gray" key={idx}><strong>{item.date}:</strong><br/>{item.info}</p>)}
         </div>
     );
 }
