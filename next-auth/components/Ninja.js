31bb1ef8da8d52569c10f1573a2993f6be0225b7
Eric <ericpeterson@tamu.edu>
2024-07-23 19:58:26 -0500

    non functional but full skeleten Ninja component

--- next-auth/components/Ninja.js
+++ next-auth/components/Ninja.js
@@ -1,13 +1,38 @@
 "use client"
 
 const Ninja = ( { data } ) => {
-  const {belt, bucks, id, imgNum, llu, name} = data
+  const {belt, bucks, id, imgNum, llu, name, ice} = data
   return (
     <div className="outerNinja">
+
       <div className={`innerLeft ${belt}`}>
         <img className='image' src={`icons/IconSquare${imgNum}.png`}/>
         <p className='ninjaName'>{name}</p>
       </div>
+
+      <div className='ninjaData'>
+
+        <p>Ninja Bucks: {bucks}</p> <br/>
+
+        Last Level Up: {llu}<br/>
+
+        {ice && <p className='emoji'>🍦</p>}
+        {!ice && <p className='emoji'>❌</p>}
+
+        <div>
+          <button className='ninja-button' >Level Up</button>
+          <button className='ninja-button' >Belt Up</button>
+          <button className='ninja-button' >Belt Down</button>
+          <button className='ninja-button' >Ice Cream</button>
+        </div>
+
+      </div>
+
+      <div>
+        <button className="editButton">✏️</button>
+        <button className="editButton">❌</button>
+      </div>
+
     </div>
   );
 }
