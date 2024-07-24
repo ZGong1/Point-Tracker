8beaf9e89d3b521f2eef03813c605fc573a6a93a
Eric <ericpeterson@tamu.edu>
2024-07-23 22:34:14 -0500

    Home button

--- next-auth/components/Header.js
+++ next-auth/components/Header.js
@@ -1,12 +1,13 @@
 "use client"
 import { logout } from "@/lib";
+import Link from "next/link";
 
 const Header = ( { loggedIn } ) => {
 
   return ( 
     <div className="header">
       <div className="left-buttons">
-          
+          <Link href="/"><button>Home</button></Link>
       </div>
       <div className="right-buttons">
         {loggedIn && <button onClick={() => logout()}>Sign out</button>}
